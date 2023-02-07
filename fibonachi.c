69fc47350581870d09952c5f13853d1e35894bfe
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2023-02-07 19:17:21 +0530

    Add files via upload

---
+++ fibonachi.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int a=0,b=1,c=0,d=0,e,f=0;
+    scanf("%d",&e);
+    while(f<e){
+        printf("%d\n",c);
+        a=b;
+        b=c;
+        c=a+b;
+        f++;
+    }
+
+}
